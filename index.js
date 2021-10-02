d5ec36aa18d3297cb11393311b5dc7006aa15950
Holly Teunis <+>
2021-10-01 21:36:37 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove()
+
+
+const newHeader = document.createElement("H1")
+newHeader.setAttribute("id", "victory")
+newHeader.innerText = "Holly is the champion"
+document.getElementById("newHeader");
+newHeader.innerHTML =
+  "Holly is the champion."
